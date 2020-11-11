4e06b0eacd8276f7f7cc76df3d32fa825a863f5a
Gregory Anders <greg@gpanders.com>
2020-11-11 09:51:54 -0700

    Finish implementing list and show subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use colored::*;
-use std::fs;
-use std::fs::DirEntry;
-use std::io::Result;
+use std::env;
+use std::fs::{self, DirEntry};
+use std::io::{self, Result, Write};
 use std::path::Path;
+use std::process::Command;
 
 fn is_hidden(entry: &DirEntry) -> bool {
     entry
@@ -11,7 +12,7 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .map_or(false, |s| s.starts_with('.'))
 }
 
-fn list(root: &Path, depth: usize) -> Result<()> {
+fn tree(root: &Path, depth: usize, prefix: String) -> Result<()> {
     let mut entries = fs::read_dir(root)?
         .filter_map(|e| e.ok())
         .filter(|e| !is_hidden(e))
@@ -20,21 +21,20 @@ fn list(root: &Path, depth: usize) -> Result<()> {
     entries.sort_by_key(|k| k.file_name());
 
     for (i, entry) in entries.iter().enumerate() {
-        for _ in 1..depth {
-            print!("│   ");
-        }
-
-        if i == entries.len() - 1 {
-            print!("└── ");
+        let is_last = i == entries.len() - 1;
+        if is_last {
+            print!("{}└── ", prefix);
         } else {
-            print!("├── ");
+            print!("{}├── ", prefix);
         }
 
         let path = entry.path();
         let file_name = entry.file_name().into_string().unwrap();
+
         if path.is_dir() {
             println!("{}", file_name.blue().bold());
-            list(path.as_path(), depth + 1)?;
+            let next_prefix = prefix.clone() + if is_last { "    " } else { "│   " };
+            tree(&path, depth + 1, next_prefix)?;
         } else {
             let file_name = match file_name.strip_suffix(".gpg") {
                 Some(e) => e,
@@ -48,9 +48,32 @@ fn list(root: &Path, depth: usize) -> Result<()> {
     Ok(())
 }
 
+fn cmd_show(store: &Path, item: &str) {
+    let item_path = String::from(store.join(item).to_str().unwrap()) + ".gpg";
+
+    let output = Command::new("gpg")
+        .arg("--decrypt")
+        .arg(item_path)
+        .output()
+        .unwrap();
+
+    io::stdout().write_all(&output.stdout).unwrap();
+}
+
+fn cmd_list(store: &Path) {
+    println!("Password Store");
+    tree(&store, 1, String::from("")).expect("Failed to list password store");
+}
+
 fn main() {
     let store = dirs::home_dir().unwrap().join(".password-store");
 
-    println!("Password Store");
-    list(store.as_path(), 1).expect("Failed to list password store");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        cmd_list(&store);
+    } else {
+        let item = &args[1];
+        cmd_show(&store, item);
+    }
 }
